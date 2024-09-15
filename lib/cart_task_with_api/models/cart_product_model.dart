27cbee5c9b6262947206f1520828a0d2169cb5e0
Iqra-Imran <iqraimran592@gmail.com>
2024-09-15 18:25:13 +0500

    cart task done

---
+++ lib/cart_task_with_api/models/cart_product_model.dart
@@ -0,0 +1,29 @@
+// lib/models/product.dart
+class ProductModel {
+  final int id;
+  final String title;
+  final double price;
+  final String description;
+  final String category;
+  final String image;
+
+  ProductModel({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.category,
+    required this.image,
+  });
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
+    return ProductModel(
+      id: json['id'],
+      title: json['title'],
+      price: (json['price'] as num).toDouble(),
+      description: json['description'],
+      category: json['category'],
+      image: json['image'],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:calculator/cart_task_with_api/provider/cart_provider.dart';
+import 'package:calculator/cart_task_with_api/screens/product_screen.dart';
 import 'package:calculator/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +14,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiProvider(providers: [
+      ChangeNotifierProvider(
+        create: (_) => CartProvider(),
+        child: const MyApp(),
+      ),
+    ],
+    child:const MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home:  HomeScreen(),
+      home:  ProductListScreen(),
+    ),
     );
+
+
   }
 }
 
